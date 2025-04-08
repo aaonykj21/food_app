6b1ac2799794e62a7bbd5db980d8ff1cea0abd26
aaonykj21 <aon.natcha29@gmail.com>
2025-04-08 16:53:25 +0700

    favorite

--- lib/pages/favorite_page.dart
+++ lib/pages/favorite_page.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class FavoritePage extends StatefulWidget {
   const FavoritePage({super.key});
@@ -8,8 +10,94 @@ class FavoritePage extends StatefulWidget {
 }
 
 class _FavoritePageState extends State<FavoritePage> {
+  List<dynamic> favorite = [];
+
+  Future<void> fetchData() async {
+    try {
+      var response = await http.get(
+        Uri.parse('http://10.0.2.2:3000/favorite'),
+      ); // ใช้ http.get() เพื่อเรียก API
+      if (response.statusCode == 200) {
+        String foodBody = utf8.decode(response.bodyBytes);
+        List<dynamic> jsonList = jsonDecode(foodBody);
+        setState(() {
+          favorite = jsonList;
+        });
+      } else {
+        throw Exception("Failed to load products");
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('รายการโปรด', style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body:
+          favorite.isEmpty
+              ? Center(child: CircularProgressIndicator())
+              : ListView.builder(
+                itemCount: favorite.length,
+                itemBuilder: (context, index) {
+                  var restaurant = favorite[index];
+                  return Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          spreadRadius: 1,
+                          blurRadius: 5,
+                        ),
+                      ],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 80,
+                            width: 80,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                image: NetworkImage(restaurant['image']),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 20),
+                          // ชื่อร้าน
+                          Expanded(
+                            child: Text(
+                              restaurant['name'],
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+    );
   }
 }
